93f974fdc1ef92f43de26cef040fefddf5954ffa
chloe guillemot <chloe.guillemot@edu.devinci.fr>
2021-03-24 16:04:36 +0100

    Add main.py and an empty shell version of book.py - End exercise 2

---
+++ book.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+class Order:
+    def __init__ (self):
+        pass
+
+class Book:
+    def __init__ (self, name):
+        pass
+
+    def insert_buy (self, quantity, price):
+        pass
+
+    def insert_sell (self, quantity, price):
+        pass
